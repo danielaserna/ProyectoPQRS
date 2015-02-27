26ae93024cb72a9e0b171a00cf10e86ab825c84e
alexis0592 <yefry.calderon@udea.edu.co>
2015-02-27 01:01:29 -0500

    add solicitudeService unitTest

---
+++ Hibernate_PQRS/test/SolicitudeServiceTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.ude.iw.PQRS.exception.IWDaoException;
+import co.edu.ude.iw.PQRS.exception.IWServiceException;
+import co.edu.udea.iw.PQRS.dao.SolicitudeDAO;
+import co.edu.udea.iw.PQRS.services.ISolicitudeService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(locations = "classpath:/co/edu/ude/iw/PQRS/spring/config/SpringConfig.xml")
+public class SolicitudeServiceTest {
+
+	@Autowired
+	private ISolicitudeService solicitudeService;
+
+	@Test
+	public void testInsertSolicitude() throws IWDaoException,
+			IWServiceException {
+
+		solicitudeService.insertSolicitude(
+				"Me salio muy malo ese producto, que cosita con ustedes",
+				"001", "queja", "1", "123456", "333");
+
+	}
+
+}
